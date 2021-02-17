ba0929922aa07785b834894f824670383587254a
georgiana chiriac <georgianachiriacweb@gmail.com>
2021-02-17 15:37:07 -0500

    create model schema, seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([
+  { name: 'The Spoon', address: '12 Leaf Rd', phone_number: '1234561234', category: 'vegan' },
+  { name: 'The Fork', address: '16 Fern Rd', phone_number: '1294571234', category: 'vegan' },
+  { name: 'Fork & Knife', address: '8 Dogwood Rd', phone_number: '1294564234', category: 'Western' },
+  { name: 'Fern', address: '4 Chateau Rd', phone_number: '12348862234', category: 'Indian' },
+  { name: 'At Maya\'s', address: '5 Temple Rd', phone_number: '1264566234', category: 'Thai' }
+])
