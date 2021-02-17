78af772c55fb3b5b97579d5bf0941a6e3c0bfc57
georgiana chiriac <georgianachiriacweb@gmail.com>
2021-02-17 16:19:29 -0500

    create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,21 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
-
+  # POST /restaurants/3/reviews
   def create
+    restaurant = Restaurant.find(params[:restaurant_id])
+    # 1. assign the form inputs
+    new_review = Review.new(review_params)
+    # 2. assign the restaurant foreign key
+    new_review.restaurant = restaurant
+    if new_review.save # if when i call "save" it returns true
+    else
+      render :new
   end
+end
+
+private
+
+def review_params
+  params.require(:review).permit(:content)
 end
